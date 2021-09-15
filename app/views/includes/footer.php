1f12d00aaecdc11e4b06c0cdbdf0ede83a051c35
Teklit <teklitte@gmail.com>
2021-09-15 12:00:51 +0200

    Feature postes

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -7,7 +7,6 @@
     </footer>
     
     <!-- Bootstrap core JS-->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="<?php echo URLROOT;?>/public/js/bootstrap.js"></script>
     <!-- Core theme JS-->
     <script src="<?php echo URLROOT;?>/public/js/script.js"></script>
