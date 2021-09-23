57eaae77b934a77af702d1a6697a64e134121283
Teklit <teklitte@gmail.com>
2021-09-23 17:46:28 +0200

    DIV - Recent posts

--- app/views/includes/home.php
+++ app/views/includes/home.php
@@ -1,46 +1,94 @@
-<div class="container-fluide  bg-dark my-4">
-    <div class="container px-5 bg-dark">
-        <div class="row g-0 px-5">
-            <div class="col-8 bg-info" >
-
-                <a href="#" class="readArticle">
+<div class="container-fluide  bg-dark">
+    <div class="container bg-dark">
+        <div class="row g-0 ">
+            <div class="col-xl-8" >
+                <a href="#" class="readArticle" target="_blank">
                     <div class="thumbnail text-center">
                         <img src="<?php echo URLROOT;?>/public/img/img-3.jpg" alt="" class="img-fluid">
-                    
-                        <div class="posttitle">
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</h3>
+                        <div class="posttitle pt-2">
+                            <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                            <p><span class="badge bg-light text-dark me-3">World</span>24/01/2021</p>
                         </div>
                     </div>
-                    
                 </a>
-
             </div>
-
-            <div class="col-4 bg-light">
-
+            <div class="col-xl-4">
                 <div class="row">
                     <div class="col-12">
-                        <a href="#" class="readArticle">
+                        <a href="#" class="readArticle" target="_blank">
                             <div class="thumbnail text-center">
-                            <img src="<?php echo URLROOT;?>/public/img/img-4.jpg" alt="" class="img-fluid">
-                            <div class="posttitle">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</h3>
+                                <img src="<?php echo URLROOT;?>/public/img/img-4.jpg" alt="" class="img-fluid">
+                                <div class="posttitle pt-2">
+                                    <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                                    <p><span class="badge bg-light text-dark me-3">News</span>24/01/2021</p>
+                                </div>
                             </div>
                         </a>
                     </div>
 
                     <div class="col-12">
-                        <a href="#" class="readArticle">
+                        <a href="#" class="readArticle" target="_blank">
                             <div class="thumbnail text-center">
-                            <img src="<?php echo URLROOT;?>/public/img/img-8.jpg" alt="" class="img-fluid">
-                            <div class="posttitle">
-                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</h3>
+                                <img src="<?php echo URLROOT;?>/public/img/img-8.jpg" alt="" class="img-fluid">
+                                <div class="posttitle">
+                                    <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                                    <p><span class="badge bg-light text-dark me-3">Editorial</span>24/01/2021</p>
+                                </div>
                             </div>
                         </a>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
+</div>
+
+<div class="container my-4">
+    <div class="row g-0">
+        <div class="col-lg-3 col-md-4">
+            <a href="#" class="readArticle" target="_blank">
+                <div class="thumbnail text-center">
+                    <img src="<?php echo URLROOT;?>/public/img/img-3.jpg" alt="" class="img-fluid">
+                    <div class="posttitle pt-2">
+                        <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                        <p><span class="badge bg-light text-dark me-3">News</span>24/01/2021</p>
+                    </div>
+                </div>
+            </a> 
+        </div>
+
+        <div class="col-lg-3 col-md-4">
+            <a href="#" class="readArticle" target="_blank">
+                <div class="thumbnail text-center">
+                    <img src="<?php echo URLROOT;?>/public/img/img-2.jpg" alt="" class="img-fluid">
+                    <div class="posttitle pt-2">
+                        <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                        <p><span class="badge bg-light text-dark me-3">News</span>24/01/2021</p>
+                    </div>
+                </div>
+            </a> 
+        </div>
+        <div class="col-lg-3 col-md-4">
+            <a href="#" class="readArticle" target="_blank">
+                <div class="thumbnail text-center">
+                    <img src="<?php echo URLROOT;?>/public/img/img-5.jpg" alt="" class="img-fluid">
+                    <div class="posttitle pt-2">
+                        <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                        <p><span class="badge bg-light text-dark me-3">News</span>24/01/2021</p>
+                    </div>
+                </div>
+            </a> 
+        </div>
+        <div class="col-lg-3 col-md-4">
+            <a href="#" class="readArticle" target="_blank">
+                <div class="thumbnail text-center">
+                    <img src="<?php echo URLROOT;?>/public/img/img-8.jpg" alt="" class="img-fluid">
+                    <div class="posttitle pt-2">
+                        <h6 class="px-3 headertitle">ነገራት ማለት ኣብ መግቢ፡ መጽለሊንክዳንን ይምርኰስ። ደቂ-ሰብ፡ </h6>
+                        <p><span class="badge bg-light text-dark me-3">News</span>24/01/2021</p>
+                    </div>
+                </div>
+            </a> 
+        </div>
+    </div>              
 </div>
--- app/views/includes/navigation.php
+++ app/views/includes/navigation.php
@@ -1,9 +1,11 @@
         <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="mx-auto navbar-nav   ">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container d-flex justify-content-center">
+                <button class="navbar-toggler  " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon "></span>
+                </button>
+                <div class="collapse navbar-collapse py-3" id="navbarSupportedContent">
+                    <ul class="mx-auto navbar-nav  text-center ">
                         <li class="nav-item me-2"><a class="nav-link active" href="<?php echo URLROOT; ?>/index">ኣዳራሽ</a></li>
                         <li class="nav-item me-2"><a class="nav-link" href="<?php echo URLROOT; ?>/blogs">ዓንቅጻት</a></li>
 
@@ -19,16 +21,16 @@
                         <li class="nav-item me-2"><a class="nav-link" href="<?php echo URLROOT; ?>/about">ብዛዕባና</a></li>
                         <li class="nav-item me-2"><a class="nav-link" href="<?php echo URLROOT; ?>/contact">ኽትረክቡና</a></li>
                     </ul>
-                    <ul class="me-5 navbar-nav bg-light  border border-light px-3">
-                        <li class="nav-item me-2">
+                    <ul class=" navbar-nav bg-dark text-center">
+                        <li class="nav-item">
                             <?php if(isset($_SESSION['user_id'])) : ?>
-                                <a class="nav-link text-dark" href="<?php echo URLROOT; ?>/users/logout">Log out</a>
+                                <a class="nav-link text-light" href="<?php echo URLROOT; ?>/users/logout">Log out</a>
                             <?php else : ?>
-                                <a class="nav-link text-dark" href="<?php echo URLROOT; ?>/users/login">ተመዝገዝቡ</a>
+                                <a class="nav-link text-light" href="<?php echo URLROOT; ?>/users/login">ተመዝገዝቡ</a>
                             <?php endif; ?>
                         </li>
                         
-                        <li class="nav-item me-2"><a class="nav-link text-dark" href="#">ሰዓቡና</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">ሰዓቡና</a></li>
                     </ul>
                 </div>
                 <hr>
